58290bf5325db3b22c31646cd797e1cf929f74d7
jchaut <j.chaut@3kles-consulting.com>
2021-07-03 23:37:26 +0200

    Update secure router

--- src/generic/router/secure.router.ts
+++ src/generic/router/secure.router.ts
@@ -9,10 +9,30 @@ export class SecureRouter extends AbstractSecureRouter {
 	public addController(controller: IGenericController, checker?: any): void {
 		if (controller) {
 			for (const key of Object.keys(controller.getParameters())) {
+				const route = (controller.getParameters()[key].path) ? controller.getParameters()[key].path : key;
 				if (controller.getParameters()[key].method) {
-					if (controller.getParameters()[key].method === 'GET') {
-						this.router.route('/' + key).post(controller.execute(key));
+					switch (controller.getParameters()[key].method) {
+						case 'GET':
+							this.router.route('/' + route).get(controller.execute(key));
+							break;
+						case 'POST':
+							this.router.route('/' + route).post(controller.execute(key));
+							break;
+						case 'PUT':
+							this.router.route('/' + route).put(controller.execute(key));
+							break;
+						case 'DELETE':
+							this.router.route('/' + route).delete(controller.execute(key));
+							break;
+						case 'PATCH':
+							this.router.route('/' + route).patch(controller.execute(key));
+							break;
+						case 'SEARCH':
+							this.router.route('/' + route).search(controller.execute(key));
+							break;
 					}
+				} else {
+					this.router.route('/' + route).get(controller.execute(key));
 				}
 			}
 		}
