eb751756769064ca6aacd21755e21b5ef1e60e57
Charles Da Silva Costa <CDASILVA@3KLES.local>
2023-02-21 16:12:28 +0100

    Add generate crud

---
+++ src/utils/crud.utils.ts
@@ -0,0 +1,58 @@
+export class CRUDUtil {
+	public static generate(route: string, hostname: string, port: number, path: string): ({ [key: string]: any }) {
+		const option = {
+			hostname,
+			port,
+			headers: {
+				'Content-Type': 'application/json',
+			}
+		};
+		return {
+			list: {
+				path: route,
+				method: 'GET',
+				option: {
+					method: 'GET',
+					path,
+					...option
+				}
+			},
+			get: {
+				path: route + '/:id',
+				method: 'GET',
+				option: {
+					path: path + '/:id',
+					method: 'GET',
+					...option
+				}
+			},
+			create: {
+				method: 'POST',
+				path: route,
+				option: {
+					path,
+					method: 'POST',
+					...option
+				}
+			},
+			update: {
+				method: 'PUT',
+				path: route + '/:id',
+				option: {
+					path: path + '/:id',
+					method: 'PUT',
+					...option
+				}
+			},
+			delete: {
+				method: 'DELETE',
+				path: route + '/:id',
+				option: {
+					path: path + '/:id',
+					method: 'DELETE',
+					...option
+				}
+			},
+		};
+	}
+}
