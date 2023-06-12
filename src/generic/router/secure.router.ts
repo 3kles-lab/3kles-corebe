0dafea41111ad829863059593906c71d2ef6a768
jchaut <j.chaut@3kles-consulting.com>
2023-06-12 19:22:06 +0200

    Fix secure router

--- src/generic/router/secure.router.ts
+++ src/generic/router/secure.router.ts
@@ -1,38 +1,41 @@
-import { AbstractGenericRouter } from './index.generic.router';
 import { IGenericController } from '../controller/index.generic.controller';
-import { IAuth } from '../auth/IAuth';
 import { AbstractSecureRouter } from './abstract.secure.router';
+import { GenericHandler } from '../index.generic';
 
 // Class to create a router from a GenericController
 export class SecureRouter extends AbstractSecureRouter {
 
 	public addController(controller: IGenericController, checker?: any): void {
 		if (controller) {
+			const handler = controller.getOption()?.handler?.handler || new GenericHandler().handler;
+
 			for (const key of Object.keys(controller.getParameters())) {
-				const route = (controller.getParameters()[key].path) ? controller.getParameters()[key].path : key;
+				const middlewares = controller.getParameters()[key].middlewares || [];
+				let route = (controller.getParameters()[key].path) ? controller.getParameters()[key].path : key;
+				route = (route.startsWith("/") ? "" : "/") + route;
 				if (controller.getParameters()[key].method) {
 					switch (controller.getParameters()[key].method) {
 						case 'GET':
-							this.router.route('/' + route).get(controller.execute(key));
+							this.router.route(route).get(...middlewares, controller.execute(key), handler);
 							break;
 						case 'POST':
-							this.router.route('/' + route).post(controller.execute(key));
+							this.router.route(route).post(...middlewares, controller.execute(key), handler);
 							break;
 						case 'PUT':
-							this.router.route('/' + route).put(controller.execute(key));
+							this.router.route(route).put(...middlewares, controller.execute(key), handler);
 							break;
 						case 'DELETE':
-							this.router.route('/' + route).delete(controller.execute(key));
+							this.router.route(route).delete(...middlewares, controller.execute(key), handler);
 							break;
 						case 'PATCH':
-							this.router.route('/' + route).patch(controller.execute(key));
+							this.router.route(route).patch(...middlewares, controller.execute(key), handler);
 							break;
 						case 'SEARCH':
-							this.router.route('/' + route).search(controller.execute(key));
+							this.router.route(route).search(...middlewares, controller.execute(key), handler);
 							break;
 					}
 				} else {
-					this.router.route('/' + route).get(controller.execute(key));
+					this.router.route('/' + route).get(...middlewares, controller.execute(key), handler);
 				}
 			}
 		}
