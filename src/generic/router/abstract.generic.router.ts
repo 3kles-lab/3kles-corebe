13fca18705f3ef57884f51ba91e3945ac1171104
JCHUAT <j.chaut@3kles-consutlting.com>
2024-08-23 18:03:12 +0200

    fix: TS2729: Property 'options' is used before its initialization

--- src/generic/router/abstract.generic.router.ts
+++ src/generic/router/abstract.generic.router.ts
@@ -4,10 +4,10 @@ import { IGenericController } from '../controller/index.generic.controller';
 
 // Class to create a router from a GenericController
 export abstract class AbstractGenericRouter implements IGenericRouter {
-	public router: express.Router = express.Router(this.options);
+	public router: express.Router;
 
 	constructor(protected options?: express.RouterOptions) {
-
+		this.router = express.Router(this.options);
 	}
 
 	public abstract addController(controller: IGenericController, checker?: any): void;
