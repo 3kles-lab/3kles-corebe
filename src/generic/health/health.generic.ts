68db5b3f23d9c59b0c540e969284b7cdca846d9e
Charles Da Silva Costa <CDASILVA@3KLES.local>
2025-05-12 14:29:16 +0200

    fix: add more information on healthcheck endpoint

--- src/generic/health/health.generic.ts
+++ src/generic/health/health.generic.ts
@@ -3,11 +3,19 @@ import { IHealth } from "./health.interface";
 export class GenericHealth implements IHealth {
 
 	public async status(): Promise<any> {
-		const version = process.env.npm_package_version;
+		const serviceVersion = process.env.SERVICE_VERSION;
+		const applicationVersion = process.env.APP_VERSION;
+		const commit = process.env.CI_COMMIT_SHORT_SHA;
+		const buildTime = process.env.APP_BUILD_TIME;
 		const used = process.memoryUsage();
+		const uptime = process.uptime();
 
 		const memory = Object.keys(used).map((key) => ({ [key]: `${Math.round(used[key] / 1024 / 1024 * 100) / 100} MB` }))
 			.reduce((a, b) => ({ ...a, ...b }), {});
-		return { version, memory };
+		return {
+			status: 'ok',
+			serviceVersion, applicationVersion,
+			commit, buildTime, memory, uptime
+		};
 	}
 }
