8e6375d0dd1913d7f02a1ec4ebb849bb367efaea
Charles Da Silva Costa <CDASILVA@3KLES.local>
2025-07-09 11:02:00 +0200

    fix: addController when serviceParams is null or undefined

--- src/generic/router/secure.router.ts
+++ src/generic/router/secure.router.ts
@@ -9,35 +9,38 @@ export class SecureRouter extends AbstractSecureRouter {
 		if (controller) {
 			const handler = controller.getOption()?.handler?.handler || new GenericHandler().handler;
 
-			for (const key of Object.keys(controller.getServiceParams())) {
-				const middlewares = controller.getServiceParams()[key].middlewares || [];
-				let route = (controller.getServiceParams()[key].path) ? controller.getServiceParams()[key].path : key;
-				route = (route.startsWith("/") ? "" : "/") + route;
-				if (controller.getServiceParams()[key].method) {
-					switch (controller.getServiceParams()[key].method) {
-						case 'GET':
-							this.router.route(route).get(...middlewares, controller.execute(key), handler);
-							break;
-						case 'POST':
-							this.router.route(route).post(...middlewares, controller.execute(key), handler);
-							break;
-						case 'PUT':
-							this.router.route(route).put(...middlewares, controller.execute(key), handler);
-							break;
-						case 'DELETE':
-							this.router.route(route).delete(...middlewares, controller.execute(key), handler);
-							break;
-						case 'PATCH':
-							this.router.route(route).patch(...middlewares, controller.execute(key), handler);
-							break;
-						case 'SEARCH':
-							this.router.route(route).search(...middlewares, controller.execute(key), handler);
-							break;
+			if(controller.getServiceParams()){
+				for (const key of Object.keys(controller.getServiceParams())) {
+					const middlewares = controller.getServiceParams()[key].middlewares || [];
+					let route = (controller.getServiceParams()[key].path) ? controller.getServiceParams()[key].path : key;
+					route = (route.startsWith("/") ? "" : "/") + route;
+					if (controller.getServiceParams()[key].method) {
+						switch (controller.getServiceParams()[key].method) {
+							case 'GET':
+								this.router.route(route).get(...middlewares, controller.execute(key), handler);
+								break;
+							case 'POST':
+								this.router.route(route).post(...middlewares, controller.execute(key), handler);
+								break;
+							case 'PUT':
+								this.router.route(route).put(...middlewares, controller.execute(key), handler);
+								break;
+							case 'DELETE':
+								this.router.route(route).delete(...middlewares, controller.execute(key), handler);
+								break;
+							case 'PATCH':
+								this.router.route(route).patch(...middlewares, controller.execute(key), handler);
+								break;
+							case 'SEARCH':
+								this.router.route(route).search(...middlewares, controller.execute(key), handler);
+								break;
+						}
+					} else {
+						this.router.route('/' + route).get(...middlewares, controller.execute(key), handler);
 					}
-				} else {
-					this.router.route('/' + route).get(...middlewares, controller.execute(key), handler);
 				}
 			}
+			
 		}
 	}
 
